8d6ee5fc3e62f5b7b9e6dbeeb841e872c10eec7b
umxe <u_habiba@rocketmail.com>
2018-06-19 19:06:19 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,16 +4,15 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors(rainbow_colors)
+def change_rainbow_colors
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
+  return rainbow_colors
 end
 
-def add_colors(rainbow_colors)
+def add_colors
   rainbow_colors.push "green"
   rainbow_colors.push "blue"
-  puts rainbow_colors
+  return rainbow_colors
 end
-change_rainbow_colors(rainbow_colors)
-add_colors(rainbow_colors)
